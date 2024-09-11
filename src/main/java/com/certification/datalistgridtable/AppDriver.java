9ce9a543ec56a67be0a953b58d7ff9bee8766f71
PauloPortofolio <pvba02@gmail.com>
2024-09-11 12:37:57 -0400

    1.0) Starting DataGrid Project
    * CONCLUDED:
    - Initial Setup
    - Git Setup
    
    * WORKING:
    - Automatic DataGrid
    - Manual DataGrid
    - DataGrid Renderers
    - DataGrid Header/Footer/Group
    - DataGrid LoadData
    
    * NEXT STEPS:
    
    * FUTURE FEATURES:

---
+++ src/main/java/com/certification/datalistgridtable/AppDriver.java
@@ -0,0 +1,23 @@
+package com.certification.datalistgridtable;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "datalistgridtable")
+public class AppDriver implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDriver.class, args);
+    }
+
+}
